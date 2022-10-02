8e749f41b741fc5784cffb4e1cca5f974eedbfca
Kirill Leyfer <k.leyfer@drweb.com>
2022-10-02 16:21:50 +0300

    Fix max HP, draw red on enemy dmg
    Signed-off-by: Kirill Leyfer <k.leyfer@drweb.com>

--- game/src/main/kotlin/org/catinthedark/jvcrplotter/game/entities/SimpleEnemy.kt
+++ game/src/main/kotlin/org/catinthedark/jvcrplotter/game/entities/SimpleEnemy.kt
@@ -2,8 +2,11 @@ package org.catinthedark.jvcrplotter.game.entities
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Circle
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.MathUtils.sin
 import com.badlogic.gdx.math.Vector2
 import org.catinthedark.jvcrplotter.lib.CoolDown
 import org.catinthedark.jvcrplotter.lib.IOC
@@ -29,6 +32,9 @@ class SimpleEnemy(
     private val initialDir = randomDir()
     private val hitCooldown = CoolDown(hitCooldownTime)
 
+    private var drawDamage = false
+    private var drawDamageTime = 0f
+
     val body: Circle
         get() = Circle(pos.x, pos.y, radius)
 
@@ -50,6 +56,26 @@ class SimpleEnemy(
         val dir = dirTo(target)
         pos.mulAdd(dir, speed.cpy().scl(Gdx.graphics.deltaTime))
 
+        if (drawDamage) {
+            drawDamageTime += Gdx.graphics.deltaTime
+            Gdx.gl.glEnable(GL20.GL_BLEND)
+            Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA)
+
+            renderer.managed(ShapeRenderer.ShapeType.Filled) {
+                val clr = Color(Color.RED)
+                val arg = drawDamageTime * MathUtils.PI * 4
+                clr.a = sin(arg) * 0.5f
+                it.color = clr
+                it.circle(pos.x, pos.y, radius)
+                if (arg > MathUtils.PI) {
+                    drawDamage = false
+                }
+            }
+            Gdx.gl.glDisable(GL20.GL_BLEND)
+        } else {
+            drawDamageTime = 0f
+        }
+
         renderer.managed(ShapeRenderer.ShapeType.Line) {
             it.color = if (!isBoss) Color.WHITE else Color.GOLD
             it.circle(pos.x, pos.y, radius)
@@ -59,6 +85,7 @@ class SimpleEnemy(
     }
 
     fun damage(bullet: Bullet) {
+        drawDamage = true
         hp -= bullet.dmg
         if (hp <= 0) {
             shouldDestroy = true
@@ -66,6 +93,7 @@ class SimpleEnemy(
     }
 
     fun damage(damage: Float) {
+        drawDamage = true
         hp -= damage
         if (hp <= 0) {
             shouldDestroy = true
