f8b99636844c64dfbf9230843728c9f9fa5e44c2
Dmitry Shatov <d.shatty@gmail.com>
2022-10-02 18:46:43 +0600

    FirePowerUp: фикс выращивания скорострельности

--- game/src/main/kotlin/org/catinthedark/jvcrplotter/game/entities/powerups/FirePowerUp.kt
+++ game/src/main/kotlin/org/catinthedark/jvcrplotter/game/entities/powerups/FirePowerUp.kt
@@ -13,8 +13,7 @@ class FirePowerUp(override var pos: Vector2) : PowerUp(pos, Color.CORAL) {
 
     override fun apply(player: Player) {
         // 1-1 2-1 2-2 3-2 3-3 4-3 4-4 5-4 6-4 7-4
-        if (player.stats.bulletsCount % 2 == 0 &&
-            player.stats.bulletsFireSpeed % 2 == 1 &&
+        if ((player.stats.bulletsCount + player.stats.bulletsFireSpeed) % 2 == 1 &&
             player.stats.bulletsFireSpeed < MAX_FIRE_SPEED
         ) {
             player.stats.bulletsFireSpeed += 1
