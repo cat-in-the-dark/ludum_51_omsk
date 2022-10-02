42f011cb7be17be2f4ac373384931f21a6ce0b15
Ilya Siganov <ilya.blan4@gmail.com>
2022-10-02 18:31:43 +0600

    add powerup collect sound

--- game/src/main/kotlin/org/catinthedark/jvcrplotter/game/entities/powerups/FirePowerUp.kt
+++ game/src/main/kotlin/org/catinthedark/jvcrplotter/game/entities/powerups/FirePowerUp.kt
@@ -2,10 +2,15 @@ package org.catinthedark.jvcrplotter.game.entities.powerups
 
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.Vector2
+import org.catinthedark.jvcrplotter.audio.Bgm
 import org.catinthedark.jvcrplotter.game.Const.Balance.PowerUp.MAX_FIRE_SPEED
 import org.catinthedark.jvcrplotter.game.entities.Player
+import org.catinthedark.jvcrplotter.lib.IOC
+import org.catinthedark.jvcrplotter.lib.atOrFail
 
 class FirePowerUp(override var pos: Vector2) : PowerUp(pos, Color.CORAL) {
+    private val bgm: Bgm by lazy { IOC.atOrFail("bgm") }
+
     override fun apply(player: Player) {
         // 1-1 2-1 2-2 3-2 3-3 4-3 4-4 5-4 6-4 7-4
         if (player.stats.bulletsCount % 2 == 0 &&
@@ -16,5 +21,7 @@ class FirePowerUp(override var pos: Vector2) : PowerUp(pos, Color.CORAL) {
         } else {
             player.stats.bulletsCount += 1
         }
+
+        bgm.collectPowerup(false)
     }
 }
--- game/src/main/kotlin/org/catinthedark/jvcrplotter/game/entities/powerups/NovaPowerUp.kt
+++ game/src/main/kotlin/org/catinthedark/jvcrplotter/game/entities/powerups/NovaPowerUp.kt
@@ -2,12 +2,17 @@ package org.catinthedark.jvcrplotter.game.entities.powerups
 
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.Vector2
+import org.catinthedark.jvcrplotter.audio.Bgm
 import org.catinthedark.jvcrplotter.game.Const
 import org.catinthedark.jvcrplotter.game.entities.NovaStats
 import org.catinthedark.jvcrplotter.game.entities.Player
+import org.catinthedark.jvcrplotter.lib.IOC
+import org.catinthedark.jvcrplotter.lib.atOrFail
 import kotlin.math.min
 
 class NovaPowerUp(override var pos: Vector2) : PowerUp(pos, Color.BLUE) {
+    private val bgm: Bgm by lazy { IOC.atOrFail("bgm") }
+
     override fun apply(player: Player) {
         val nova = player.stats.nova
         if (nova != null) {
@@ -23,5 +28,7 @@ class NovaPowerUp(override var pos: Vector2) : PowerUp(pos, Color.BLUE) {
         } else {
             player.stats.nova = NovaStats()
         }
+
+        bgm.collectPowerup(false)
     }
 }
