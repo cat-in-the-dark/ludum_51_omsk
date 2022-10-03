af74f8ca6ef6833afbde511009e6e40661925e9e
Kirill Leyfer <k.leyfer@drweb.com>
2022-10-03 22:42:39 +0300

    Visualize tower damage
    Signed-off-by: Kirill Leyfer <k.leyfer@drweb.com>

--- game/src/main/kotlin/org/catinthedark/alyoep/game/entities/Tower.kt
+++ game/src/main/kotlin/org/catinthedark/alyoep/game/entities/Tower.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.math.Vector2
 import org.catinthedark.alyoep.game.Const
 import org.catinthedark.alyoep.lib.*
@@ -29,6 +30,10 @@ class Tower(override val pos: Vector2, override var shouldDestroy: Boolean = fal
     private val beaconRadius = Vector2(0f, 20f)
 
     private var radius = Vector2(0f, Const.Balance.Tower.RADIUS)
+
+    private var drawDamage = false
+    private var drawDamageTime = 0f
+
     val p1: Vector2
         get() = pos.cpy().add(radius.cpy().scl(1f, 0.5f))
     val p2: Vector2
@@ -41,6 +46,10 @@ class Tower(override val pos: Vector2, override var shouldDestroy: Boolean = fal
     }
 
     private fun draw() {
+        if (drawDamage) {
+            drawDamageTime += Gdx.graphics.deltaTime
+        }
+
         val p1Cached = p1
         val p2Cached = p2
         val p3Cached = p3
@@ -52,11 +61,28 @@ class Tower(override val pos: Vector2, override var shouldDestroy: Boolean = fal
         val bp4 = beaconPoint.cpy().add(beaconRadius.cpy().rotateDeg(270f).scl(1f, 0.5f))
         val points = listOf(bp1, bp2, bp3, bp4)
 
+        if (drawDamage) {
+            points.forEach {
+                val jitterX = MathUtils.map(0f, 1f, -2f, 2f, MathUtils.random())
+                val jitterY = MathUtils.map(0f, 1f, -2f, 2f, MathUtils.random())
+                it.add(Vector2(jitterX, jitterY))
+            }
+        }
+
         Gdx.gl.glEnable(GL20.GL_BLEND)
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA)
         render.managed(ShapeRenderer.ShapeType.Filled) {
-            val color = Color.FIREBRICK
-            color.a = 0.5f
+            val color = Color(Color.FIREBRICK)
+            if (drawDamage) {
+                val arg = drawDamageTime * MathUtils.PI * 4
+                color.a = MathUtils.sin(arg) * 0.5f
+                if (arg > MathUtils.PI) {
+                    drawDamage = false
+                }
+            } else {
+                color.a = 0.5f
+                drawDamageTime = 0f
+            }
             it.color = color
 
             val hpScale = 1f - currentHP / Const.Balance.Tower.MAX_HP
@@ -103,6 +129,7 @@ class Tower(override val pos: Vector2, override var shouldDestroy: Boolean = fal
     }
 
     fun hit(damage: Float) {
+        drawDamage = true
         currentHP -= damage
         if (currentHP <= 0) {
             currentHP = 0f
